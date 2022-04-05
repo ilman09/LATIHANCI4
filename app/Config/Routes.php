6813b573b1216f2c5536ac42d6ff76f7887cec07
ilman09 <zidnidardiri09@gmail.com>
2022-04-05 09:06:18 +0700

    four commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,9 +31,11 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'HelloController::index');
-
+$routes->get('/', 'login::index');
 $routes->resource('product');
+
+$routes->resource('register');
+$routes->resource('login');
 
 /*
  * --------------------------------------------------------------------
